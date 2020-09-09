6e74e1e3f8ef72a95774a85924caddbeb2558ae5
Vsevolod Ganin <vsga@yandex-team.ru>
2020-09-09 22:59:55 +0300

    Attach automatic gain control to click audio

--- app/src/main/java/net/ganin/vsevolod/clicktrack/audio/ClickTrackPlayer.kt
+++ app/src/main/java/net/ganin/vsevolod/clicktrack/audio/ClickTrackPlayer.kt
@@ -2,9 +2,11 @@ package net.ganin.vsevolod.clicktrack.audio
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.media.audiofx.AutomaticGainControl
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import net.ganin.vsevolod.clicktrack.R
+import net.ganin.vsevolod.clicktrack.audio.ClickTrackPlayer.Const.CLICK_MINIMAL_INTERVAL
 import net.ganin.vsevolod.clicktrack.lib.ClickTrack
 import net.ganin.vsevolod.clicktrack.lib.CueWithDuration
 import kotlin.coroutines.coroutineContext
@@ -14,11 +16,13 @@ class ClickTrackPlayer(private val context: Context) {
 
     suspend fun play(clickTrack: ClickTrack) {
         val mediaPlayer = MediaPlayer.create(context, R.raw.click)
+        val agc = mediaPlayer.tryAttachAgc()
         try {
             do {
                 clickTrack.cues.forEach { mediaPlayer.play(it) }
             } while (clickTrack.loop && coroutineContext.isActive)
         } finally {
+            agc?.release()
             mediaPlayer.release()
         }
     }
@@ -26,11 +30,19 @@ class ClickTrackPlayer(private val context: Context) {
     private suspend fun MediaPlayer.play(cueWithDuration: CueWithDuration) {
         val delay = 1.minutes / cueWithDuration.cue.bpm
         var leftToPlay = cueWithDuration.durationInTime
-        while (leftToPlay.toLongMilliseconds() > 1) {
+        while (leftToPlay > CLICK_MINIMAL_INTERVAL) {
             start()
             leftToPlay -= delay
             // FIXME: Fix NoSuchMethod and use override with kotlin.time directly
             delay(delay.toLongMilliseconds())
         }
+    }
+
+    private fun MediaPlayer.tryAttachAgc(): AutomaticGainControl? {
+        return AutomaticGainControl.create(audioSessionId)
+    }
+
+    private object Const {
+        val CLICK_MINIMAL_INTERVAL = 1.minutes / 1000 // Max. 1000 bpm
     }
 }
