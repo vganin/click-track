433b40d39c9ded45d9e6db9dfd87f6d4b7c5d8bd
Vsevolod Ganin <vsga@yandex-team.ru>
2020-09-11 02:51:56 +0300

    Apply default material theme

--- app/src/main/java/net/ganin/vsevolod/clicktrack/view/ContentView.kt
+++ app/src/main/java/net/ganin/vsevolod/clicktrack/view/ContentView.kt
@@ -1,9 +1,12 @@
 package net.ganin.vsevolod.clicktrack.view
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import net.ganin.vsevolod.clicktrack.lib.ClickTrack
 
 @Composable
 fun ContentView(clickTrack: ClickTrack, onPlayToggle: (Boolean) -> Unit) {
-    ClickTrackAndPlayStopView(clickTrack = clickTrack, onPlayToggle = onPlayToggle)
+    MaterialTheme {
+        ClickTrackAndPlayStopView(clickTrack = clickTrack, onPlayToggle = onPlayToggle)
+    }
 }
