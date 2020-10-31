e3e003ecba5dc2c75cbb0f83f401320dbfa7bee0
Vsevolod Ganin <vsga@yandex-team.ru>
2020-10-31 17:05:39 +0300

    Revamp DurationTypeDropdown

--- app/src/main/java/net/ganin/vsevolod/clicktrack/view/widget/EditCueDurationView.kt
+++ app/src/main/java/net/ganin/vsevolod/clicktrack/view/widget/EditCueDurationView.kt
@@ -1,23 +1,36 @@
 package net.ganin.vsevolod.clicktrack.view.widget
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.ripple.RippleIndication
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Position
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import net.ganin.vsevolod.clicktrack.R
 import net.ganin.vsevolod.clicktrack.lib.CueDuration
 import net.ganin.vsevolod.clicktrack.lib.SerializableDuration
-import java.util.Locale
 import kotlin.time.Duration
 import kotlin.time.minutes
 import kotlin.time.seconds
@@ -75,33 +88,52 @@ private fun DurationTypeDropdown(
     modifier: Modifier,
 ) {
     val toggleState = remember { mutableStateOf(false) }
+    val onToggleClick = { toggleState.value = !toggleState.value }
+
     DropdownMenu(
+        toggleModifier = modifier
+            .clickable(
+                onClick = onToggleClick,
+                indication = RippleIndication()
+            )
+            .width(DROPDOWN_TOGGLE_WIDTH),
         toggle = {
-            Row(modifier = modifier.clickable(onClick = {
-                toggleState.value = !toggleState.value
-            })) {
-                Arrow(direction = ArrowDirection.DOWN)
-                Text(text = state.value.displayText.capitalize(Locale.US))
+            Row(
+                modifier = Modifier.padding(start = 16.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = stringResource(id = state.value.displayStringResId),
+                        style = MaterialTheme.typography.subtitle1,
+                    )
+                }
+                IconButton(onClick = onToggleClick) {
+                    Icon(Icons.Default.ArrowDropDown)
+                }
             }
         },
         expanded = toggleState.value,
         onDismissRequest = { toggleState.value = false },
-    ) {
-        CueDurationType.values().forEach { durationType ->
-            val selectedState = remember { mutableStateOf(state.value == durationType) }
-            Text(
-                text = "As ${durationType.displayText}",
-                modifier = Modifier.selectable(
-                    selected = selectedState.value,
-                    onClick = {
-                        state.value = durationType
-                        toggleState.value = false
-                    }
-                )
-            )
+        dropdownModifier = Modifier.width(DROPDOWN_TOGGLE_WIDTH),
+        dropdownOffset = Position(-DROPDOWN_TOGGLE_WIDTH, 0.dp),
+        dropdownContent = {
+            CueDurationType.values().forEach { durationType ->
+                DropdownMenuItem(onClick = {
+                    state.value = durationType
+                    toggleState.value = false
+                }) {
+                    Text(text = stringResource(id = durationType.displayStringResId))
+                }
+            }
         }
-    }
+    )
 }
+
+private val DROPDOWN_TOGGLE_WIDTH = 140.dp
 
 @Composable
 private fun EditBeatsView(
@@ -115,7 +147,7 @@ private fun EditBeatsView(
             Arrow(ArrowDirection.LEFT)
         }
         Text(
-            text = "${beatsCountState.value} beats",
+            text = beatsCountState.value.toString(),
             modifier = Modifier.align(Alignment.CenterVertically)
         )
         Button(onClick = { beatsCountState.value += 1 }) {
@@ -138,7 +170,7 @@ private fun EditTimeView(
             Arrow(ArrowDirection.LEFT)
         }
         Text(
-            text = "${durationState.value.inMinutes} minutes",
+            text = durationState.value.inMinutes.toString(),
             modifier = Modifier.align(Alignment.CenterVertically)
         )
         Button(onClick = { durationState.value += 10.seconds }) {
@@ -149,10 +181,19 @@ private fun EditTimeView(
     state.value = CueDuration.Time(SerializableDuration(durationState.value))
 }
 
-private enum class CueDurationType(val displayText: String) {
-    BEATS("beats"),
-    TIME("time"),
+private enum class CueDurationType {
+    BEATS,
+    TIME,
 }
+
+@get:StringRes
+private val CueDurationType.displayStringResId: Int
+    get() {
+        return when (this) {
+            CueDurationType.BEATS -> R.string.cue_duration_beats
+            CueDurationType.TIME -> R.string.cue_duration_minutes
+        }
+    }
 
 private val CueDuration.type: CueDurationType
     get() {
