339590cd908ce4b6abe4dd60062ad03944fe1b7d
Vsevolod Ganin <vsga@yandex-team.ru>
2020-11-01 22:43:54 +0300

    Redo duration picker

--- app/src/main/java/net/ganin/vsevolod/clicktrack/view/widget/DurationPicker.kt
+++ app/src/main/java/net/ganin/vsevolod/clicktrack/view/widget/DurationPicker.kt
@@ -1,8 +1,7 @@
 package net.ganin.vsevolod.clicktrack.view.widget
 
+import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -10,16 +9,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.focus
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.isFocused
+import androidx.compose.ui.focusObserver
+import androidx.compose.ui.focusRequester
+import androidx.compose.ui.gesture.tapGestureFilter
+import androidx.compose.ui.platform.TextInputServiceAmbient
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.BackspaceKeyEditOp
+import androidx.compose.ui.text.input.CommitTextEditOp
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.InputSessionToken
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.OffsetMap
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.MinutesPerHour
+import androidx.compose.ui.unit.SecondsPerMinute
 import androidx.ui.tooling.preview.Preview
 import net.ganin.vsevolod.clicktrack.R
 import java.text.DecimalFormat
@@ -33,24 +38,19 @@ import kotlin.time.seconds
 fun DurationPicker(
     state: MutableState<Duration>,
 ) {
-    fun CharSequence.safeSubSequence(startIndex: Int, endIndex: Int): CharSequence {
-        val range = 0..length
-        return subSequence(startIndex.coerceIn(range), endIndex.coerceIn(range))
-    }
-
-    /** Converts CharSequence in format "ssmmhh*" to Duration */
+    /** Converts CharSequence in format "hhmmss" to Duration */
     fun CharSequence.toDuration(): Duration {
-        val secondsSequence = safeSubSequence(0, 2)
-        val minutesSequence = safeSubSequence(2, 4)
-        val hoursSequence = safeSubSequence(4, length)
+        val hoursSequence = subSequence(0, 2)
+        val minutesSequence = subSequence(2, 4)
+        val secondsSequence = subSequence(4, 6)
         val sequenceToTimeMultiplier = listOf(
-            secondsSequence to 1,
-            minutesSequence to 60,
-            hoursSequence to 60 * 60,
+            hoursSequence to SecondsPerMinute * MinutesPerHour,
+            minutesSequence to SecondsPerMinute,
+            secondsSequence to 1L,
         )
 
         val radix = 10
-        var seconds = 0
+        var seconds = 0L
         for ((sequence, timeMultiplier) in sequenceToTimeMultiplier) {
             var baseMultiplier = 1
             for (char in sequence.reversed()) {
@@ -63,148 +63,97 @@ fun DurationPicker(
         return seconds.seconds
     }
 
-    /** Converts Duration to String in format "ssmmhh*" */
+    /** Converts Duration to String in format "hhmmss" */
     fun Duration.asString(): String {
-        val twoDigitsFormat = DecimalFormat("00")
-        fun Int.twoDigits() = twoDigitsFormat.format(this)
         return toComponents { hours, minutes, seconds, _ ->
-            when {
-                hours > 0 -> "${seconds.twoDigits()}${minutes.twoDigits()}${hours}"
-                minutes > 0 -> "${seconds.twoDigits()}${minutes}"
-                else -> "$seconds"
-            }
+            "${hours.coerceAtMost(99).twoDigits()}${minutes.twoDigits()}${seconds.twoDigits()}"
         }
     }
 
-    fun String.hasNonDigitChars(): Boolean {
-        return any { !it.isDigit() }
+    val internalStringState = remember { mutableStateOf(state.value.asString()) }
+
+    if (internalStringState.value.toDuration() != state.value) {
+        internalStringState.value = state.value.asString()
     }
 
-    var text by remember { mutableStateOf(state.value.asString()) }
-    var selection by remember { mutableStateOf(TextRange.Zero) }
-
-    val secondsString = stringResource(R.string.duration_picker_seconds)
-    val minutesString = stringResource(R.string.duration_picker_minutes)
-    val hoursString = stringResource(R.string.duration_picker_hours)
-
-    text = when (text) {
-        "", "0" -> "0"
-        else -> text.trimStart('0')
-    }
-    selection = when (text) {
-        "", "0" -> TextRange(1)
-        else -> selection.constrain(0, text.length)
+    fun enterDigit(char: Char) {
+        if (char.isDigit() && internalStringState.value.first() == '0') {
+            internalStringState.value = internalStringState.value.drop(1) + char
+        }
+        state.value = internalStringState.value.toDuration()
     }
 
-    TextField(
-        value = TextFieldValue(
-            text = text,
-            selection = selection
-        ),
+    fun removeDigit() {
+        internalStringState.value = '0' + internalStringState.value.dropLast(1)
+        state.value = internalStringState.value.toDuration()
+    }
+
+    val inputService = TextInputServiceAmbient.current!!
+    var inputSessionToken: InputSessionToken? by remember { mutableStateOf(null) }
+    val focusRequester = FocusRequester()
+
+    @Composable
+    fun formatInternalState(): String {
+        val hoursString = stringResource(R.string.duration_picker_hours)
+        val minutesString = stringResource(R.string.duration_picker_minutes)
+        val secondsString = stringResource(R.string.duration_picker_seconds)
+
+        return StringBuilder().apply {
+            append(internalStringState.value.subSequence(0, 2))
+            append(hoursString)
+            append(' ')
+            append(internalStringState.value.subSequence(2, 4))
+            append(minutesString)
+            append(' ')
+            append(internalStringState.value.subSequence(4, 6))
+            append(secondsString)
+        }.toString()
+    }
+
+    Text(
+        text = formatInternalState(),
         modifier = Modifier
-            .width(with(DensityAmbient.current) { 128.sp.toDp() }),
-        onValueChange = { newValue ->
-            val newText = newValue.text
-            val newSelection = newValue.selection
-            if (!newText.hasNonDigitChars() && newText.length < 7) {
-                state.value = newText.toDuration()
-                text = newText
-                selection = newSelection
+            .focusRequester(focusRequester)
+            .focusObserver { focusState ->
+                if (focusState.isFocused && inputSessionToken == null) {
+                    inputSessionToken = inputService.startInput(
+                        value = TextFieldValue(),
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done,
+                        onEditCommand = { operations ->
+                            operations.forEach { operation ->
+                                when (operation) {
+                                    is BackspaceKeyEditOp -> removeDigit()
+                                    is CommitTextEditOp -> operation.text.forEach(::enterDigit)
+                                }
+                            }
+                        },
+                        onImeActionPerformed = { action ->
+                            if (action == ImeAction.Done) {
+                                focusRequester.freeFocus()
+                            }
+                        }
+                    )
+                } else if (!focusState.isFocused && inputSessionToken != null) {
+                    inputSessionToken?.let(inputService::stopInput)
+                    inputSessionToken = null
+                }
             }
-        },
-        keyboardType = KeyboardType.Number,
-        visualTransformation = object : VisualTransformation {
-            override fun filter(text: AnnotatedString): TransformedText {
-                @Suppress("NAME_SHADOWING")
-                val text = text.text
-
-                val originalToTransformed = mutableListOf<Int>()
-                val transformedToOriginal = mutableListOf<Int>()
-
-                val transformedText = StringBuilder().apply {
-                    var originalTextIndex = 0
-                    var transformedTextIndex = 0
-
-                    fun appendCharBindingToIndex(char: Char, originalTextIndex: Int) {
-                        transformedToOriginal += originalTextIndex
-                        append(char)
-                        transformedTextIndex = length
-                    }
-
-                    fun appendStringBindingToIndex(string: String, originalTextIndex: Int) {
-                        repeat(string.length) {
-                            transformedToOriginal += originalTextIndex
-                        }
-                        append(string)
-                        transformedTextIndex = length
-                    }
-
-                    fun appendNextOriginalChar(transformedTextIndex: Int) {
-                        if (originalTextIndex < text.length) {
-                            val char = text[originalTextIndex]
-                            originalToTransformed += transformedTextIndex
-                            appendCharBindingToIndex(char, originalTextIndex)
-                            ++originalTextIndex
-                        }
-                    }
-
-                    appendNextOriginalChar(transformedTextIndex)
-                    appendNextOriginalChar(transformedTextIndex)
-                    var transformedTextAfterSectionIndex = transformedTextIndex
-                    appendCharBindingToIndex(' ', originalTextIndex)
-                    appendStringBindingToIndex(secondsString, originalTextIndex)
-                    if (text.length > 2) {
-                        appendCharBindingToIndex(' ', originalTextIndex)
-                        appendNextOriginalChar(transformedTextAfterSectionIndex)
-                        appendNextOriginalChar(transformedTextIndex)
-                        transformedTextAfterSectionIndex = transformedTextIndex
-                        appendCharBindingToIndex(' ', originalTextIndex)
-                        appendStringBindingToIndex(minutesString, originalTextIndex)
-                    }
-                    if (text.length > 4) {
-                        appendCharBindingToIndex(' ', originalTextIndex)
-                        appendNextOriginalChar(transformedTextAfterSectionIndex)
-                        appendNextOriginalChar(transformedTextIndex)
-                        transformedTextAfterSectionIndex = transformedTextIndex
-                        appendCharBindingToIndex(' ', originalTextIndex)
-                        appendStringBindingToIndex(hoursString, originalTextIndex)
-                    }
-
-                    originalToTransformed += transformedTextAfterSectionIndex
-                    transformedToOriginal += originalTextIndex
-                }.toString()
-
-                return TransformedText(
-                    transformedText = AnnotatedString(transformedText),
-                    offsetMap = object : OffsetMap {
-                        override fun originalToTransformed(offset: Int): Int {
-                            return originalToTransformed[offset.coerceIn(originalToTransformed.indices)]
-                        }
-
-                        override fun transformedToOriginal(offset: Int): Int {
-                            return transformedToOriginal[offset.coerceIn(transformedToOriginal.indices)]
-                        }
-                    }
-                )
-            }
-        }
+            .focus()
+            .tapGestureFilter { focusRequester.requestFocus() },
     )
 }
 
-private fun TextRange.constrain(minimumValue: Int, maximumValue: Int): TextRange {
-    val newStart = start.coerceIn(minimumValue, maximumValue)
-    val newEnd = end.coerceIn(minimumValue, maximumValue)
-    if (newStart != start || newEnd != end) {
-        return TextRange(newStart, newEnd)
-    }
-    return this
-}
+private val twoDigitsFormat = DecimalFormat("00")
+private fun Int.twoDigits() = twoDigitsFormat.format(this)
 
 @Preview
 @Composable
 fun PreviewTimePicker() {
+    val sharedState = remember { mutableStateOf(1.hours + 2.minutes + 3.seconds + 4.milliseconds) }
     Column {
-        DurationPicker(mutableStateOf(1.hours + 2.minutes + 3.seconds + 4.milliseconds))
+        DurationPicker(sharedState)
+        DurationPicker(sharedState)
         DurationPicker(mutableStateOf(1.minutes + 2.seconds + 3.milliseconds))
         DurationPicker(mutableStateOf(1.seconds + 2.milliseconds))
         DurationPicker(mutableStateOf(1.milliseconds))
