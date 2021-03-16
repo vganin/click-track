d73439ee2d0fd0a16ac1367ab056ad254f2ee960
Vsevolod Ganin <vsga@yandex-team.ru>
2021-03-17 00:34:17 +0300

    Add dividers to list chooser

--- app/src/main/java/com/vsevolodganin/clicktrack/view/widget/settings/ListChooser.kt
+++ app/src/main/java/com/vsevolodganin/clicktrack/view/widget/settings/ListChooser.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -17,7 +18,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEachIndexed
 import com.vsevolodganin.clicktrack.utils.compose.toUpperCase
 
 data class ListChooserItem<T>(
@@ -58,27 +61,83 @@ fun <T> ListChooser(
                 }
             },
             text = {
-                Column {
-                    variants.forEach { item ->
-                        Column(modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                showChooser = false
-                                onChoose(item.value)
-                            }
-                            .padding(8.dp)
-                        ) {
-                            Text(text = item.displayValue)
-                            if (item.description != null) {
-                                Text(
-                                    text = item.description,
-                                    style = MaterialTheme.typography.caption,
-                                )
-                            }
-                        }
+                DialogContent(
+                    variants = variants,
+                    onChoose = { value ->
+                        showChooser = false
+                        onChoose(value)
                     }
-                }
+                )
             }
         )
     }
 }
+
+@Composable
+private fun <T> DialogContent(
+    variants: List<ListChooserItem<T>>,
+    onChoose: (T) -> Unit,
+) {
+    Column {
+        variants.fastForEachIndexed { index, item ->
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    onChoose(item.value)
+                }
+                .padding(8.dp)
+            ) {
+                Text(text = item.displayValue)
+                if (item.description != null) {
+                    Text(
+                        text = item.description,
+                        style = MaterialTheme.typography.caption,
+                    )
+                }
+            }
+
+            if (index != variants.lastIndex) {
+                Divider()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    ListChooser(
+        title = "Number option",
+        value = PreviewData[1].displayValue,
+        variants = PreviewData,
+        onChoose = {},
+        modifier = Modifier,
+    )
+}
+
+@Preview
+@Composable
+private fun DialogPreview() {
+    DialogContent(
+        variants = PreviewData,
+        onChoose = {},
+    )
+}
+
+private val PreviewData = listOf(
+    ListChooserItem(
+        value = 0,
+        displayValue = "Zero",
+        description = "Just regular zero",
+    ),
+    ListChooserItem(
+        value = 1,
+        displayValue = "One",
+        description = "More sophisticated number - a one",
+    ),
+    ListChooserItem(
+        value = 2,
+        displayValue = "Two",
+        description = "Yep, that's a fucking two",
+    ),
+)
