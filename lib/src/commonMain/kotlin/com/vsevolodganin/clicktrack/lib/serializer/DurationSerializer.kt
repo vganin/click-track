88d7f499b68fbf04ac0cd385b0d584ca2c29fa2b
Vsevolod Ganin <vsga@yandex-team.ru>
2021-06-07 15:36:53 +0300

    Remove redundant annotation

--- lib/src/commonMain/kotlin/com/vsevolodganin/clicktrack/lib/serializer/DurationSerializer.kt
+++ lib/src/commonMain/kotlin/com/vsevolodganin/clicktrack/lib/serializer/DurationSerializer.kt
@@ -3,14 +3,12 @@ package com.vsevolodganin.clicktrack.lib.serializer
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-@Serializer(forClass = Duration::class)
 internal object DurationSerializer : KSerializer<Duration> {
 
     override val descriptor: SerialDescriptor
