383dcce65b0205655582c092f98f48725fe607db
Vsevolod Ganin <vsga@yandex-team.ru>
2021-08-17 21:47:39 +0300

    Fix compilation

--- lib/src/commonMain/kotlin/com/vsevolodganin/clicktrack/lib/TwoLayerPolyrhythm.kt
+++ lib/src/commonMain/kotlin/com/vsevolodganin/clicktrack/lib/TwoLayerPolyrhythm.kt
@@ -1,5 +1,6 @@
 package com.vsevolodganin.clicktrack.lib
 
+import com.vsevolodganin.clicktrack.lib.android.AndroidIgnoredOnParcel
 import com.vsevolodganin.clicktrack.lib.android.AndroidParcelable
 import com.vsevolodganin.clicktrack.lib.android.AndroidParcelize
 import kotlin.time.Duration
@@ -12,6 +13,8 @@ public data class TwoLayerPolyrhythm(
     val layer1: Int,
     val layer2: Int,
 ) : AndroidParcelable {
+
+    @AndroidIgnoredOnParcel
     val durationInTime: Duration by lazy {
         bpm.interval * layer1
     }
