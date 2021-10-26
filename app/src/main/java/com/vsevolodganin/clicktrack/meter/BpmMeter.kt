0ff5e1df9387463ed721c9d60e2e11169b16545f
Vsevolod Ganin <vsga@yandex-team.ru>
2021-10-26 23:54:14 +0300

    Fix correctness bug in bpm tap meter

--- app/src/main/java/com/vsevolodganin/clicktrack/meter/BpmMeter.kt
+++ app/src/main/java/com/vsevolodganin/clicktrack/meter/BpmMeter.kt
@@ -7,36 +7,35 @@ import kotlin.time.Duration
 
 class BpmMeter @Inject constructor() {
 
-    private val lock = Any()
     private val samples: Array<Long?> = Array(HISTORY_SIZE) { null }
     private var index: Int = 0
 
-    fun addTap() = synchronized(lock) {
+    fun addTap() {
         val now = now()
-        index = (index + 1) % HISTORY_SIZE
         samples[index] = now
+        index = (index + 1) % HISTORY_SIZE
     }
 
-    fun calculateBpm(): BeatsPerMinute? = synchronized(lock) {
+    fun calculateBpm(): BeatsPerMinute? {
         val now = now()
         val applicableSamples = samples
             .asSequence()
             .filterNotNull()
-            .filter { now - it < TIME_WINDOW_SIZE }
+            .filter { now - it < TIME_WINDOW_SIZE_MILLIS }
             .toList()
             .takeIf { it.size >= MIN_COUNT_TO_DERIVE_BPM }
             ?: return null
         val minTime = applicableSamples.minOrNull() ?: return null
         val maxTime = applicableSamples.maxOrNull() ?: return null
         return BeatsPerMinute(
-            beatCount = applicableSamples.size,
-            timelapse = Duration.milliseconds((maxTime - minTime)),
+            beatCount = applicableSamples.size - 1,
+            timelapse = Duration.milliseconds(maxTime - minTime),
         )
     }
 
     private fun now() = SystemClock.uptimeMillis()
 }
 
-private const val TIME_WINDOW_SIZE = 3000L // ms
-private const val HISTORY_SIZE = 6
-private const val MIN_COUNT_TO_DERIVE_BPM = 2
+private const val TIME_WINDOW_SIZE_MILLIS = 3000L
+private const val HISTORY_SIZE = 10
+private const val MIN_COUNT_TO_DERIVE_BPM = 3
