d86f65f059ea073714f051b3e4ea7176b0495291
Vsevolod Ganin <vsga@yandex-team.ru>
2022-07-29 01:20:22 +0300

    Add build type to check performance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,12 @@ android {
 
             manifestPlaceholders += "crashlyticsCollectionEnabled" to "false"
         }
+
+        create("perftest") {
+            initWith(getByName("release"))
+            applicationIdSuffix = ".perftest"
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
 
     sourceSets["main"].java {
