60a9619d6d78e426378c6550c9acdc3fc21325d4
vsga <vsevolod.ganin@gmail.com>
2024-04-19 23:18:06 +0100

    Fix drag handle color

--- multiplatform/src/commonMain/kotlin/com/vsevolodganin/clicktrack/ui/piece/DragHandle.kt
+++ multiplatform/src/commonMain/kotlin/com/vsevolodganin/clicktrack/ui/piece/DragHandle.kt
@@ -2,13 +2,14 @@ package com.vsevolodganin.clicktrack.ui.piece
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DragIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -22,7 +23,7 @@ fun DragHandle(
         Icon(
             imageVector = Icons.Default.DragIndicator,
             contentDescription = null,
-            tint = Color(0).copy(alpha = 0.2f)
+            tint = LocalContentColor.current.copy(alpha = ContentAlpha.medium)
         )
     }
 }
