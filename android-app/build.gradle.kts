fa66c158476c59167c91bec27a5715c20c1b3d5e
vsga <vsevolod.ganin@gmail.com>
2024-04-20 00:33:22 +0100

    Use PAT to make version bump commit push instead of making a PR

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -26,14 +26,12 @@ if (keystorePropertiesFile.exists()) {
 val baseVersion = "1.1.1"
 val currentDate = SimpleDateFormat("yyyyMMdd").format(Date())!!
 
-val versionCodeFile = file("version-code")
-
 android {
     namespace = "com.vsevolodganin.clicktrack"
 
     defaultConfig {
         applicationId = "com.vsevolodganin.clicktrack"
-        versionCode = versionCodeFile.readText().trim().toInt()
+        versionCode = file("version-code").readText().trim().toInt()
         versionName = "$baseVersion ($currentDate)"
 
         resourceConfigurations += setOf("en", "ru")
@@ -163,11 +161,4 @@ dependencies {
     implementation(libs.firebase.crashlytics.ndk)
     implementation(libs.googlePlay.core)
     ksp(libs.kotlininject.compiler)
-}
-
-tasks.register("bumpAppVersionCode") {
-    doLast {
-        val versionCode = versionCodeFile.readText().trim().toInt()
-        versionCodeFile.writeText("${versionCode + 1}")
-    }
 }
