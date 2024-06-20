b23a0145c7c52a9d4d8b73fa0b7fdd05e41dcbc0
Vsevolod Ganin <vganin@users.noreply.github.com>
2024-06-20 20:40:25 +0100

    Set ktlint version to 1.3.0 (#75)

--- build-src/src/main/kotlin/clicktrack.ktlint.gradle.kts
+++ build-src/src/main/kotlin/clicktrack.ktlint.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint")
 }
 
+ktlint {
+    version = "1.3.0"
+}
+
 //dependencies {
 //    //noinspection UseTomlInstead
 //    ktlintRuleset("io.nlopez.compose.rules:ktlint:0.4.4")
