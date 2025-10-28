1f8f89f3209bee9117be9b94098f47f5156ae304
Vsevolod Ganin <vganin@users.noreply.github.com>
2025-10-28 11:36:20 +0000

    Enable 16KB alignment (#169)

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -41,7 +41,10 @@ android {
 
         externalNativeBuild {
             cmake {
-                arguments("-DANDROID_STL=c++_shared")
+                arguments += listOf(
+                    "-DANDROID_STL=c++_shared",
+                    "-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON",
+                )
             }
         }
     }
