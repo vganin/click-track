0f6aa1f3a1539ba379f57b1b2f5210a148128cad
vsga <vsevolod.ganin@gmail.com>
2025-10-28 14:00:11 +0000

    Remove Firebase Analytics

--- android-app/src/main/java/com/vsevolodganin/clicktrack/di/module/FirebaseModule.kt
+++ android-app/src/main/java/com/vsevolodganin/clicktrack/di/module/FirebaseModule.kt
@@ -1,17 +1,10 @@
 package com.vsevolodganin.clicktrack.di.module
 
-import android.app.Application
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.vsevolodganin.clicktrack.di.component.ApplicationScope
 import me.tatarka.inject.annotations.Provides
 
 interface FirebaseModule {
-    @Provides
-    @ApplicationScope
-    fun provideFirebase(application: Application): FirebaseAnalytics {
-        return FirebaseAnalytics.getInstance(application)
-    }
 
     @Provides
     @ApplicationScope
