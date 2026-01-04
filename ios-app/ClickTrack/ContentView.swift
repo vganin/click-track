78b7f57c5319e1eb15366c9b47f55efacd08e6e0
Vsevolod Ganin <vganin@users.noreply.github.com>
2026-01-04 17:45:13 +0000

    Save and restore state in iOS app (#226)

--- ios-app/ClickTrack/ContentView.swift
+++ ios-app/ClickTrack/ContentView.swift
@@ -4,9 +4,13 @@ import ClickTrackMultiplatform
 struct ContentView: UIViewControllerRepresentable {
     
     let applicationComponent: ApplicationComponent
+    let decomposeComponentContext: DecomposeComponentContext
     
     func makeUIViewController(context: Context) -> some UIViewController {
-        return MainKt.createMainViewController(applicationComponent: applicationComponent)
+        return MainKt.createMainViewController(
+            applicationComponent: applicationComponent,
+            componentContext: decomposeComponentContext,
+        )
     }
 
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
--- multiplatform/src/iosMain/kotlin/com/vsevolodganin/clicktrack/Main.kt
+++ multiplatform/src/iosMain/kotlin/com/vsevolodganin/clicktrack/Main.kt
@@ -1,16 +1,18 @@
 package com.vsevolodganin.clicktrack
 
-import com.arkivanov.decompose.DefaultComponentContext
-import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.decompose.ComponentContext
 import com.vsevolodganin.clicktrack.di.component.ApplicationComponent
 import dev.zacsweers.metro.createGraph
 import platform.UIKit.UIViewController
 
 fun createApplicationComponent(): ApplicationComponent = createGraph()
 
-fun createMainViewController(applicationComponent: ApplicationComponent): UIViewController {
+fun createMainViewController(
+    applicationComponent: ApplicationComponent,
+    componentContext: ComponentContext,
+): UIViewController {
     return applicationComponent
         .mainViewApplicationComponentFactory
-        .create(DefaultComponentContext(LifecycleRegistry()))
+        .create(componentContext)
         .mainViewController
 }
